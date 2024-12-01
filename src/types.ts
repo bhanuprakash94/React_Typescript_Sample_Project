1906671b614259c0b11aa05db7f29b810ab715c6
bhanuprakash94 <venkatabhanuprakash.n@gmail.com>
2024-12-01 10:18:26 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,14 @@
 export interface UserInterface {
-    id: number;
-    [key: string]: string | number;
+  id: number;
+  first_name: string;
+  last_name: string;
+  name: string;
+  email: string;
 }
 
 export interface Userschema {
-    name: string;
-    display_name: string;
-    type: string;
-    required: boolean;
+  field_name: keyof UserInterface;
+  display_name: string;
+  type: string;
+  required: boolean;
 }
